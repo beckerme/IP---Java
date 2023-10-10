4a8a6a5ffb2b528063a2d6df01203a2462708c50
Ewerton Becker <75760847+beckerme@users.noreply.github.com>
2023-10-10 10:17:25 +0100

    Continuacao da ficha 2

---
+++ f_2.java
@@ -0,0 +1,19 @@
+package Ficha_IP;
+
+public class f_2 {
+
+	public f_2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		//ficha 2 - num 6
+		int hora = 1;
+		int minutos = 0;
+		int segundos = 0;
+		int conversaoParaSegundos = (3600 * hora) + (60 * minutos) + segundos;
+		System.out.println("Segundos: " + conversaoParaSegundos);
+		
+	}
+
+}
