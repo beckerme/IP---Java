244191ce41629065d5e7922585ed28d946068b93
Ewerton Becker <75760847+beckerme@users.noreply.github.com>
2023-10-13 10:49:26 +0100

    Ficha 2
    Exercicios da ficha 2

---
+++ IP/src/Ficha2/Ficha2_8.java
@@ -0,0 +1,12 @@
+package Ficha_IP;
+
+public class Ficha2_8 {
+
+	public static void main(String[] args) {
+		double graus = 120, pi = Math.PI, grados = 90;
+		double conversaoGraus = graus * pi / 180;
+		double conversaoGrados = graus * 100 / grados;
+		System.out.printf("%.3f radianos, %.3f grados",conversaoGraus, conversaoGrados);
+	}
+
+}
